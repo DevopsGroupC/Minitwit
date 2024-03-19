06796dfe13d35977f89afc857388b35ef5408f2d
Mathias Gleitze <mathiasghoffmann@gmail.com>
2024-03-19 13:10:21 +0100

    Added Repositories to delegate db functionality out from controllers.

---
+++ csharp-minitwit/ActionFilters/AuthorizeSessionAttribute .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace csharp_minitwit.ActionFilters;
+
+public class AsyncSessionAuthorizeAttribute : Attribute, IAsyncAuthorizationFilter
+{
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        await Task.Run(() => {
+            var userId = context.HttpContext.Session.GetInt32("user_id");
+
+            // If no user ID is found in the session, set the result to unauthorized
+            if (!userId.HasValue)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        });
+    }
+}
+
---
+++ csharp-minitwit/Services/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using csharp_minitwit.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_minitwit.Services.Repositories
+{
+    public class UserRepository(MinitwitContext dbContext) : IUserRepository
+    {
+        private static readonly PasswordHasher<User> PasswordHasher = new();
+
+        public async Task<int?> GetUserIdAsync(string username)
+        {
+            return await dbContext.Users
+                .Where(u => u.Username == username)
+                .Select(u => (int?)u.UserId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<User?> GetByUsername(string username)
+        {
+            return await dbContext.Users
+                .Where(u => u.Username == username)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            return await dbContext.Users.AnyAsync(u => u.Username == username);
+        }
+
+        public async Task<bool> InsertUser(string username, string email, string password)
+        {
+            try
+            {
+                var user = new User
+                {
+                    Username = username,
+                    Email = email,
+                };
+
+                user.PwHash = PasswordHasher.HashPassword(user, password);
+
+                dbContext.Users.Add(user);
+                await dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Todo: Add logging
+                return false;
+            }
+        }
+    }
+}
