4b129a3abb38e909a3455405a6510eee7fc381e4
Mathias Gleitze <mathiasghoffmann@gmail.com>
2024-03-19 13:12:53 +0100

    Switching to ORM using repositories.

---
+++ csharp-minitwit/Models/DTOs/FollowActionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace csharp_minitwit.Models.DTOs
+{
+    public class FollowActionDto
+    {
+        public string? Follow { get; set; }
+        public string? Unfollow { get; set; }
+    }
+}
--- csharp-minitwit/Services/IDatabaseService.cs
+++
@@ -1,6 +0,0 @@
-namespace csharp_minitwit.Services;
-
-public interface IDatabaseService
-{
-    Task<IEnumerable<T>> QueryDb<T>(string sqlQuery, Dictionary<string, object> parameters);
-}
