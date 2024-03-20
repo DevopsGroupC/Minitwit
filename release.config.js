56ad0c2c0fd19e8684555ee7fb2c97659a9b6391
eb <eleb@itu.dk>
2024-03-20 22:17:38 +0100

    feat: workflow test

--- release.config.js
+++ release.config.js
@@ -1,15 +1,23 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+
 const config = {
-    branches: ['feature/68-automatic-release-workflow'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/npm',
-      ['@semantic-release/git', {
-        'assets': ['package.json'],
-        'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }],
-      '@semantic-release/github'
-    ]
-  };
-  
-  module.exports = config;
+  branches: ["feature/68-automatic-release-workflow"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+}
+
+module.exports = config
