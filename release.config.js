ace3d7e815d99a217d19eb44feddf17e50348a27
eb <eleb@itu.dk>
2024-03-20 22:59:36 +0100

    fix: comment out nom, since we do not have to publish to the npm registry

--- release.config.js
+++ release.config.js
@@ -7,7 +7,7 @@ const config = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
+    // "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
