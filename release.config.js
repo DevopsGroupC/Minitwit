a7f9d302656424473f427b9fdb0406b7bbb33cb9
eb <eleb@itu.dk>
2024-03-20 23:18:41 +0100

    feat: removing the npm mark and testing

--- release.config.js
+++ release.config.js
@@ -7,7 +7,6 @@ const config = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    // "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
